167b65749600007ce1153e55b3dc678cf2742448
PedroAbreuHS <104274261+PedroAbreuHS@users.noreply.github.com>
2023-08-25 11:43:45 -0300

    Final

---
+++ src/mock/tasks.mock.js
@@ -0,0 +1,32 @@
+export const DB = [
+  {
+    id: 1,
+    title: "Trabalhar",
+    description: "Iniciar a jornada de trabalho as 08:00pm",
+    completed: true,
+  },
+  {
+    id: 2,
+    title: "Limpar Casa",
+    description: "Agendar visita da diarista",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Exercitar",
+    description: "Ir a academia realizar exercícios físicos",
+    completed: false,
+  },
+  {
+    id: 4,
+    title: "Jogar Video Game",
+    description: "Aproveitar o lançamento do MK1",
+    completed: true,
+  },
+  {
+    id: 5,
+    title: "Lavar Carro",
+    description: "Levar o carro ao lava jato",
+    completed: true,
+  },
+];
